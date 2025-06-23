98eefa51a3e07235d6feb1bee53b1e305c1fd3f4
teerawin <teerapol.th@gmail.com>
2025-06-23 15:49:33 +0700

    Update functionFile.js

--- functionFile.js
+++ functionFile.js
@@ -1,14 +1,7 @@
-
 function onMessageSendHandler(event) {
   Office.context.ui.displayDialogAsync("https://teerawin.github.io/wechecklap/confirm.html",
-    { height: 30, width: 20 },
+    { height: 30, width: 20, displayInIframe: true },
     function (asyncResult) {
-      const dialog = asyncResult.value;
-      dialog.addEventHandler(Office.EventType.DialogMessageReceived, function (arg) {
-        const result = arg.message === "yes";
-        event.completed({ allowEvent: result });
-        dialog.close();
-      });
-    }
-  );
+      event.completed({ allowEvent: true }); // หรือตาม logic จริง
+    });
 }
