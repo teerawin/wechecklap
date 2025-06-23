ab8a4d5c9e6f67c87f9e6c2e82666aef6005d249
teerawin <teerapol.th@gmail.com>
2025-06-23 14:21:29 +0700

    Add files via upload

--- functionFile.js
+++ functionFile.js
@@ -1,4 +1,14 @@
+
 function onMessageSendHandler(event) {
-  // just pass
-  event.completed({ allowEvent: true });
+  Office.context.ui.displayDialogAsync("https://teerawin.github.io/wechecklap/confirm.html",
+    { height: 30, width: 20 },
+    function (asyncResult) {
+      const dialog = asyncResult.value;
+      dialog.addEventHandler(Office.EventType.DialogMessageReceived, function (arg) {
+        const result = arg.message === "yes";
+        event.completed({ allowEvent: result });
+        dialog.close();
+      });
+    }
+  );
 }
